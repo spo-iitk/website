0c26a99b4ceac8148ad1c486f7f455a025904565
Abhishek Shree <abhi1234456@gmail.com>
2022-04-05 00:56:20 +0530

    Fix Hero

--- views/HomePage/Hero.tsx
+++ views/HomePage/Hero.tsx
@@ -15,11 +15,16 @@ export default function Hero() {
   return (
     <HeroWrapper>
       <Contents>
-        <CustomOverTitle>SPO IIT Kanpur</CustomOverTitle>
-        <Heading>About Us</Heading>
+        <CustomOverTitle>IIT Kanpur</CustomOverTitle>
+        <Heading>Students' Placement Office</Heading>
         <Description>
-        The Indian Institute of Technology Kanpur is well known for its academic excellence and is often considered as the 'first stop' for a large number of industries and other organizations for recruiting undergraduate and post-graduate students. We truly appreciate the faith bestowed on us from several organizations and we look forward to continuing our relationship with them.
-        The Students' Placement Office (SPO) is run and managed by an efficient team of office staff & students and handles all aspects of placements at IIT Kanpur. Right from contacting companies to managing all logistics of arranging for tests, pre-placement talks and conducting final interviews the SPO team provides it's best possible assistance to the recruiters. The hospitality and the functioning of SPO is applauded and well received by the visiting recruiters every year.
+          The Indian Institute of Technology Kanpur is well known for its academic excellence and is often considered as the 'first stop'
+          for a large number of industries and other organizations for recruiting undergraduate and post-graduate students. We truly
+          appreciate the faith bestowed on us from several organizations and we look forward to continuing our relationship with them. The
+          Students' Placement Office (SPO) is run and managed by an efficient team of office staff & students and handles all aspects of
+          placements at IIT Kanpur. Right from contacting companies to managing all logistics of arranging for tests, pre-placement talks
+          and conducting final interviews the SPO team provides it's best possible assistance to the recruiters. The hospitality and the
+          functioning of SPO is applauded and well received by the visiting recruiters every year.
         </Description>
         {/* <CustomButtonGroup>
           <Button onClick={() => setIsModalOpened(true)}>
@@ -97,7 +102,7 @@ const CustomOverTitle = styled(OverTitle)`
 `;
 
 const Heading = styled.h1`
-  font-size: 7.2rem;
+  font-size: 4.5rem;
   font-weight: bold;
   line-height: 1.1;
   margin-bottom: 4rem;
