a0fc3530cff882da030000e9c2c7bdd2b6113aa4
Harshit Raj <75195728+1-Harshit@users.noreply.github.com>
2022-05-20 00:11:55 +0530

    Add inpage nav in `/contact` (#79)

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -5,9 +5,19 @@ import { media } from "utils/media"
 import FormSection from "views/ContactPage/FormSection"
 import InformationSection from "views/ContactPage/InformationSection"
 
+const Routes = [
+	{
+		name: "Contact Placement Coordinators",
+		url: "/placement-coordinators"
+	}, {
+		name: "Contact Administration",
+		url: "/administration"
+	}
+]
+
 export default function ContactPage() {
 	return (
-		<Page title="Contact" description="Feel free to reach out to us!">
+		<Page title="Contact" description="Feel free to reach out to us!" routes={Routes}>
 			<ContactContainer>
 				<InformationSection />
 				<FormSection />
