99fcf29d84e6c1c85c0cf49e2a23a53389cbbd32
Abhishek Shree <abhi1234456@gmail.com>
2022-05-31 00:31:36 +0530

    Update NewsIIT.tsx

--- views/HomePage/NewsIIT.tsx
+++ views/HomePage/NewsIIT.tsx
@@ -5,21 +5,23 @@ import { media } from "utils/media"
 import Container from "components/Container"
 import Link from "next/link"
 import SectionTitle from "components/SectionTitle"
+import Head from "next/head"
 
 export default function NewsIIT(){
     return (
+	<Head>
+        	<script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
+        	<script async defer crossOrigin="anonymous" src="https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v14.0" nonce="DiXLrsY8"></script>
+      	</Head>
         <Section title = "">
         <SectionTitle>News@IITK</SectionTitle>
-        <br />
-			  <br /> 
+        <br /> <br /> 
         <Wrapper>
         <TwitterWrapper>    
         <a className ="twitter-timeline" data-width="400" data-height="300" href="https://twitter.com/IITKanpur?ref_src=twsrc%5Etfw">Tweets by IITKanpur</a> 
-        <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
         </TwitterWrapper>
         <FacebookWrapper>
         <div id="fb-root"></div>
-        <script async defer crossOrigin="anonymous" src="https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v14.0" nonce="DiXLrsY8"></script>
         <div className ="fb-page" data-href="https://www.facebook.com/iitkanpur" data-tabs="timeline" data-width="500" data-height="300" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/iitkanpur" className = "fb-xfbml-parse-ignore">
         <a href="https://www.facebook.com/iitkanpur">Indian Institute of Technology Kanpur</a></blockquote></div>
         </FacebookWrapper>
